1ba7a1c88091057e6d90054bbea241ec13d9bc8e
Ilyes Gharbi <ilyes.gharbi@ulb.be>
2023-08-15 11:50:58 +0200

    Add aac testing loopfunction

---
+++ loop-functions/SwarmAC/AACTestLoopFunc.h
@@ -0,0 +1,42 @@
+/*
+ * Aggregation with Ambient Clues
+ *
+ * @author Antoine Ligot - <aligot@ulb.ac.be>
+ */
+
+#ifndef CHOCOLATE_AAC_LOOP_FUNC_H
+#define CHOCOLATE_AAC_LOOP_FUNC_H
+
+#include "../../../src/CoreLoopFunctions.h"
+#include <argos3/core/simulator/space/space.h>
+#include <argos3/plugins/robots/e-puck/simulator/epuck_entity.h>
+
+using namespace argos;
+
+class AACTestLoopFunc : public CoreLoopFunctions {
+
+   public:
+      AACTestLoopFunc();
+      AACTestLoopFunc(const AACTestLoopFunc& orig);
+      virtual ~AACTestLoopFunc();
+
+      virtual void Destroy();
+      virtual void Reset();
+      virtual void PostStep();
+      virtual void PostExperiment();
+
+      Real GetObjectiveFunction();
+
+      virtual CColor GetFloorColor(const CVector2& c_position_on_plane);
+
+      virtual CVector3 GetRandomPosition();
+
+    private:
+      Real m_fRadius;
+      CVector2 m_cCoordBlackSpot;
+      CVector2 m_cCoordWhiteSpot;
+
+      Real m_fObjectiveFunction;
+};
+
+#endif
